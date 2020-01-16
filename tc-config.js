24bc396bafaf293a5d04dc57699c687c0bba6129
Papadzul <cabaisdennis@yahoo.com>
2020-01-16 22:55:04 +0400

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://J0icpfyEmfOG04GDUj0JEaU94aEd5h4e_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "How to Find Reliable Sources"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
